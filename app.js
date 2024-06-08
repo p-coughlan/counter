6a11ffd050c6526dbc2f55bd2c08ad1145e132db
p-coughlan <patrickcoughlan@yahoo.co.uk>
2024-06-08 16:13:55 +0100

    add notes for readability and maintenance

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-// set initial count
+// set initial count to 0
 let count = 0
 
-//select value and buttons
+//select value and buttons using querySelector on id and class
 const value =document.querySelector('#value');
 const btns = document.querySelectorAll('.btn');
 
+//console logging during development to see what is being selected
 //console.log(btns);
 // NodeList(3) [button.btn.decrease, button.btn.reset, button.btn.increase]
 
-//select all buttons
+//select all buttons and loop through them using forEach method
 btns.forEach(function (btn) {
-    console.log(btn);
-    //add event listener to each button, the event is click (e is the event object)
+
+    //add event listener to each button. The event is click (e is the event object)
     btn.addEventListener('click', function (e) {
-        const styles = e.currentTarget.classList; 
+        const styles = e.currentTarget.classList; //currentTarget is the button that is clicked. classList is the class of the button
+
+        //if else statement to check which button is clicked and update the count accordingly
         if (styles.contains ('decrease')) {
             count--; 
         }
@@ -23,7 +26,8 @@ btns.forEach(function (btn) {
         else { //reset
             count = 0;
         }
-
+        
+        //if statement to change the color of the count based on the value of the count
         if (count > 0) {
             value.style.color = 'green';
         }
